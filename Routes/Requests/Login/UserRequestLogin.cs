689506549589b9ee5e10cc50747b2c394b30ad84
Luegiu Yuusha-sama <cassianofpereira@gmail.com>
2025-09-23 20:54:29 -0300

    O Login Request estava sem Required

--- Routes/Requests/Login/UserRequestLogin.cs
+++ Routes/Requests/Login/UserRequestLogin.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace apiserasa.routes.requests.login;
 
 public class UserRequestLogin
 {
+    [Required]
     public string Email { get; set; } = default!;
+    [Required]
     public string Password { get; set; } = default!;
 }
